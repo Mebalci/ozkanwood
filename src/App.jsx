6ab83e2d3cd1dd2355243fba3dc4efe8cdd9c181
Muhammed Emin BALCI <102964743+Mebalci@users.noreply.github.com>
2025-08-08 15:16:19 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -7,26 +6,31 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import ProductList from './components/ProductList';
 import ProductDetail from './pages/ProductDetail';
+import CartPage from './pages/CartPage'; // yeni
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Header />
-        <main className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/hakkimizda" element={<About />} />
-            <Route path="/iletisim" element={<Contact />} />
-            <Route path="/urunler" element={<ProductList />} />
-            <Route path="/urun/:id" element={<ProductDetail />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div className="d-flex flex-column min-vh-100">
+          <Header />
+          <main className="flex-grow-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/hakkimizda" element={<About />} />
+              <Route path="/iletisim" element={<Contact />} />
+              <Route path="/urunler" element={<ProductList />} />
+              <Route path="/urun/:id" element={<ProductDetail />} />
+              <Route path="/sepet" element={<CartPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
