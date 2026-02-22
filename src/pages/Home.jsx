67d4ad30d9d9b3cbc0c6fb2b36af2beca4f1fdeb
Muhammed Emin BALCI <102964743+Mebalci@users.noreply.github.com>
2026-02-22 20:30:16 +0300

    Change image format from .wp2 to .webp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { urunleriGetir } from "../api";
-import arka from "../assets/ARKA.wp2";
+import arka from "../assets/ARKA.webp";
 import logoBeyaz from "../assets/LOGO_BEYAZ.png";
 
 
