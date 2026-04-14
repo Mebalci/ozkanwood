cf6c23455531d6c7fc0be41d93a0b71dac17163b
Muhammed Emin BALCI <102964743+Mebalci@users.noreply.github.com>
2026-04-14 18:06:44 +0300

    Add kategorileriGetir function to fetch categories

--- src/api.js
+++ src/api.js
@@ -3,3 +3,9 @@ export const urunleriGetir = async () => {
   const data = await response.json();
   return data.products || [];
 };
+
+export const kategorileriGetir = async () => {
+  const response = await fetch("/kategoriler.json");
+  const data = await response.json();
+  return data.categories || [];
+};
